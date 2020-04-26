bebec24a54ad9d3e6cf10fa32c1c0c4baa2027ab
MkItSimple <choanania1@gmail.com>
2020-04-26 20:22:05 +0800

    08-GalleryIntentandstartActivityForResult

--- app/src/main/java/com/example/kotlinespresso2/data/source/MoviesDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.example.kotlinespresso2.data.source
-
-import com.example.kotlinespresso2.data.Movie
-
-interface MoviesDataSource {
-
-    fun getMovie(movieId: Int): Movie?
-}
--- app/src/main/java/com/example/kotlinespresso2/ui/Placeholder.kt
+++
@@ -1,2 +0,0 @@
-package com.example.kotlinespresso2.ui
-
